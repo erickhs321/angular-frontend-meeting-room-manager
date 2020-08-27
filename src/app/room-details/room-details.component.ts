f3ea21212bfacf54f1d0c289a15d85e12547005c
Erick Henrique <erickhenrique321@gmail.com>
2020-08-27 13:39:53 -0300

    room details component done

--- src/app/room-details/room-details.component.ts
+++ src/app/room-details/room-details.component.ts
@@ -1,15 +1,37 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit, Input } from '@angular/core';
+import { Room } from '../room';
+import { RoomService } from '../room.service';
+import { RoomListComponent } from '../room-list';
+import { Router, ActivatedRoute } from '@angular/router';
 @Component({
   selector: 'app-room-details',
   templateUrl: './room-details.component.html',
-  styleUrls: ['./room-details.component.scss']
+  styleUrls: ['./room-details.component.scss'],
 })
 export class RoomDetailsComponent implements OnInit {
+  id: number;
+  room: Room;
 
-  constructor() { }
+  constructor(
+    private route: ActivatedRoute,
+    private router: Router,
+    private RoomService: RoomService
+  ) {}
 
-  ngOnInit(): void {
+  ngOnInit() {
+    this.room = new Room();
+    this.id = this.route.snapshot.params['id'];
+
+    this.RoomService.getRoom(this.id).subscribe(
+      (data) => {
+        console.log(data);
+        this.room = data;
+      },
+      (error) => console.log(error)
+    );
   }
 
+  list() {
+    this.router.navigate(['rooms']);
+  }
 }
